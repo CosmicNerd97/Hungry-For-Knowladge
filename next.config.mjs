4ccbbd2974dfb6f4bfcf70a1155ceb9b20c9f76d
Yuwen-0 <halil.alagul0@gmail.com>
2024-06-26 15:59:57 +0300

    add:placeholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["placehold.co"],
+  },
+  experimental: {
+    // Disable the default security mechanisms for SVGs in Next.js 13
+    // This is necessary because we use external SVGs that are not sanitized
+    // by Cloudflare Access
+    dangerouslyAllowSVG: true,
+  },
+};
 
 export default nextConfig;
